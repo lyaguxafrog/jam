1a98e49d7bdb828fcb61dffa5fc5c72577914c08
Makridenko Adrian <adrian.makridenko@proton.me>
2025-01-09 14:07:14 +0300

    [+] generation refresh token

--- jam/jwt/tools.py
+++ jam/jwt/tools.py
@@ -4,15 +4,18 @@ import base64
 import hashlib
 import hmac
 import json
-import secrets  # noqa
-import time  # noqa
+import secrets
+import time
 
 from jam.config import JAMConfig
 
 
 def __gen_access_token__(config: JAMConfig, payload: dict) -> str:
 
-    __payload__: dict = {"data": payload, "exp": config.JWT_ACCESS_EXP}
+    __payload__: dict = {
+        "data": payload,
+        "exp": time.time() + config.JWT_ACCESS_EXP,
+    }
 
     encoded_header: str = (
         base64.urlsafe_b64encode(
@@ -40,3 +43,39 @@ def __gen_access_token__(config: JAMConfig, payload: dict) -> str:
         f"{encoded_header}.{encoded_payload}.{encoded_signature}"
     )
     return access_token
+
+
+def __gen_refresh_token__(config: JAMConfig, payload: dict) -> str:
+
+    __payload__: dict = {
+        "data": payload,
+        "exp": time.time() + config.JWT_REFRESH_EXP,
+        "jit": secrets.token_hex(16),
+    }
+
+    encoded_header: str = (
+        base64.urlsafe_b64encode(
+            json.dumps({"alg": config.JWT_ALGORITHM, "typ": "JWT"}).encode()
+        )
+        .decode()
+        .rstrip("=")
+    )
+    encoded_payload: str = (
+        base64.urlsafe_b64encode(json.dumps(__payload__).encode())
+        .decode()
+        .rstrip("=")
+    )
+
+    __signature__: bytes = hmac.new(
+        config.JWT_REFRESH_SECRET_KEY.encode(),
+        f"{encoded_header}.{encoded_payload}".encode(),
+        hashlib.sha256,
+    ).digest()
+    encoded_signature: str = (
+        base64.urlsafe_b64encode(__signature__).decode().rstrip("=")
+    )
+
+    refresh_token: str = (
+        f"{encoded_header}.{encoded_payload}.{encoded_signature}"
+    )
+    return refresh_token
