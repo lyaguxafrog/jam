3acf822f60a1f7623856605dccf090070b5e61b2
Makridenko Adrian <adrian.makridenko@proton.me>
2025-03-10 14:56:58 +0300

    [*] Fix instacnce init

--- jam/__core_staffs__.py
+++ jam/__core_staffs__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from abc import ABC, abstractmethod
-from typing import Dict, Literal
+from typing import Literal
 
 from jam.jwt.__tools__ import __gen_jwt__
 
@@ -10,8 +10,8 @@ class AbstractConfig(ABC):
     @abstractmethod
     def __init__(
         self,
-        JWT_SECRET_KEY: str | None,
-        JWT_PRIVATE_KEY: str | None,
+        JWT_SECRET_KEY: str | None = None,
+        JWT_PRIVATE_KEY: str | None = None,
         JWT_ALGORITHM: (
             Literal[
                 "HS256",
@@ -50,9 +50,9 @@ class AbstractIntance(ABC):
     def __init__(self, config: AbstractConfig) -> None:
         self.config = config
 
-    @abstractmethod
-    def gen_jwt_tokens(self, **kwargs) -> Dict[str, str]:
-        raise NotImplementedError
+    # @abstractmethod
+    # def gen_jwt_tokens(self, **kwargs) -> Dict[str, str]:
+    #     raise NotImplementedError
 
     @abstractmethod
     def gen_token(self, **kwargs) -> str:
@@ -66,21 +66,19 @@ class Config(AbstractConfig):
 
     def __init__(
         self,
-        JWT_SECRET_KEY,
-        JWT_PRIVATE_KEY,
+        JWT_SECRET_KEY=None,
+        JWT_PRIVATE_KEY=None,
         JWT_ALGORITHM=None,
         JWT_ACCESS_EXP=3600,
         JWT_REFRESH_EXP=3600,
         JWT_HEADERS=None,
     ):
-        super().__init__(
-            JWT_SECRET_KEY,
-            JWT_ALGORITHM,
-            JWT_PRIVATE_KEY,
-            JWT_ACCESS_EXP,
-            JWT_REFRESH_EXP,
-            JWT_HEADERS,
-        )
+        self.JWT_SECRET_KEY = JWT_SECRET_KEY
+        self.JWT_ALGORITHM = JWT_ALGORITHM
+        self.JWT_PRIVATE_KEY = JWT_PRIVATE_KEY
+        self.JWT_ACCESS_EXP = JWT_ACCESS_EXP
+        self.JWT_REFRESH_EXP = JWT_REFRESH_EXP
+        self.JWT_HEADERS = JWT_HEADERS
 
 
 class Jam(AbstractIntance):
