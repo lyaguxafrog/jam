13ed8b1508fb272662296f7be076bed0b293689e
Makridenko Adrian <adrian.makridenko@proton.me>
2025-03-11 11:48:16 +0300

    [+] Util for rsa keys

--- jam/utils/__init__.py
+++ jam/utils/__init__.py
@@ -1,1 +1,3 @@
 # -*- coding: utf-8 -*-
+
+from .rsa import generate_rsa_key_pair
--- jam/utils/rsa.py
+++ jam/utils/rsa.py
@@ -1,11 +1,41 @@
 # -*- coding: utf-8 -*-
 
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 
 
-def gen_keys(*, key_size: int = 2048, public_exponent: int = 65537):
+def generate_rsa_key_pair(key_size: int = 2048) -> dict:
+    """
+    RSA key generation utility
+
+    Args:
+        key_size (int): Size of RSA key
+
+    Returns:
+        (dict): with public and private keys in format:
+
+    ```python
+    {
+        "public": "some_key",
+        "private": "key"
+    }
+    ```
+    """
     private_key = rsa.generate_private_key(
-        public_exponent=public_exponent, key_size=key_size
+        public_exponent=65537, key_size=key_size, backend=default_backend()
     )
 
-    return private_key
+    public_key = private_key.public_key()
+
+    pem_private = private_key.private_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PrivateFormat.TraditionalOpenSSL,
+    )
+
+    pem_public = public_key.public_bytes(
+        encoding=serialization.Encoding.PEM,
+        format=serialization.PublicFormat.SubjectPublicKeyInfo,
+    )
+
+    return {"public": pem_public, "private": pem_private}
