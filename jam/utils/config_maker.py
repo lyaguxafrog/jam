12936aac25b13c48e34b1be3afd8ae195a19de29
Makridenko Adrian <adrianmakridenko@duck.com>
2025-04-04 13:12:57 +0300

    [+] Config maker utils

---
+++ jam/utils/config_maker.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from typing import Any, Literal
+
+from jam.exceptions.jwt import EmptyPublicKey, EmptySecretKey, EmtpyPrivateKey
+
+
+def make_jwt_config(
+    alg: Literal[
+        "HS256",
+        "HS384",
+        "HS512",
+        "RS256",
+        "RS384",
+        "RS512",
+        # "PS256",
+        # "PS384",
+        # "PS512",
+    ] = "HS256",
+    secret_key: str | None = None,
+    public_key: str | None = None,
+    private_key: str | None = None,
+    expire: int = 3600,
+) -> dict[str, Any]:
+    """Util for making JWT config.
+
+    Args:
+        alg (Literal["HS256", "HS384", "HS512", "RS256", "RS384", "RS512", "PS512", "PS384", "PS512"]): Algorithm for token encryption
+        secret_key (str | None): Secret key for HMAC enecryption
+        private_key (str | None): Private key for RSA enecryption
+        public_key (str | None): Public key for RSA
+        expire (int): Token lifetime in seconds
+
+    Raises:
+        EmptySecretKey: If HS* algorithm is selected, but the secret key is empty
+        EmtpyPrivateKey: If RS* algorithm is selected, but the private key is empty
+        EmtpyPublicKey: If RS* algorithm is selected, but the public key is empty
+    """
+    if alg.startswith("HS") and secret_key is None:
+        raise EmptySecretKey
+
+    if alg.startswith("RS") and private_key is None:
+        raise EmtpyPrivateKey
+
+    if alg.startswith("RS") and public_key is None:
+        raise EmptyPublicKey
+
+    return {
+        "alg": alg,
+        "secret_key": secret_key,
+        "private_key": private_key,
+        "public_key": public_key,
+        "expire": expire,
+    }
