cfb3ee544886721d5ce3f8f48a07f03b29001a03
Adrian Makridenko <adrianmakridenko@duck.com>
2025-08-26 11:42:32 +0300

    [*] Update redis methods

--- jam/sessions/redis.py
+++ jam/sessions/redis.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import datetime
 import json
 from typing import Callable, Optional, Union
 from uuid import uuid4
@@ -69,60 +68,47 @@ class RedisSessions(BaseSessionModule):
         Returns:
             str: The unique ID of the created session.
         """
-        session_id = self.id
+        session_id = self._encode_session_id_if_needed(
+            f"{session_key}:{self.id}"
+        )
+        logger.debug("Gen session: %s", session_id)
         self._redis.hset(
             name=f"{self.session_path}:{session_key}",
             key=session_id,
             value=json.dumps(data),
         )
-        self._redis.hexpire(
-            f"{self.session_path}:{session_key}",
-            self.ttl,
-            session_id,
-        )
+        logger.debug("Set session %s successfully.", session_id)
+        if self.ttl:
+            self._redis.hexpire(
+                f"{self.session_path}:{session_key}", self.ttl, session_id
+            )
+            logger.debug(
+                "Set TTL for session %s to %d seconds.", session_id, self.ttl
+            )
 
-        if hasattr(self, "_code_session_key"):
-            session_id = self._encode_session_id(f"{session_key}:{session_id}")
-            return session_id
+        return session_id
 
-        return f"{session_key}:{session_id}"
-
-    def get(
-        self, session_id: str, delete_if_expire: bool = True
-    ) -> dict | None:
+    def get(self, session_id: str) -> dict | None:
         """Retrieve a session by its key or ID.
 
         Args:
             session_id (str): The session key or ID.
-            delete_if_expire (bool): If True, delete the session if it has expired. Defaults to True.
 
         Returns:
             dict | None: The session data if found, otherwise None.
         """
-        if hasattr(self, "_code_session_key"):
-            try:
-                session_id = self._decode_session_id(session_id)
-            except ValueError as e:
-                logger.error("Failed to decode session ID: %s", e)
-                return None
-
-        session_key, session_id_ = session_id.split(":", 1)
-        data = self._redis.hget(
-            f"{self.session_path}:{session_key}", session_id_
+        decoded_session_key = self._decode_session_id_if_needed(
+            session_id
+        ).split(":", 1)
+        session = self._redis.hget(
+            name=f"{self.session_path}:{decoded_session_key[0]}",
+            key=session_id,
         )
-
-        if not data:
+        if not session:
+            logger.debug("Session %s not found.", session_id)
             return None
 
-        data = json.loads(data)
-        if (
-            "_session_exp" in data
-            and datetime.datetime.now().timestamp() > data["_session_exp"]
-        ):
-            if delete_if_expire:
-                self.delete(session_id)
-            return None
-
+        data = json.loads(session)
         return data
 
     def delete(self, session_id: str) -> None:
@@ -131,16 +117,13 @@ class RedisSessions(BaseSessionModule):
         Args:
             session_id (str): The session ID.
         """
-        if hasattr(self, "_code_session_key"):
-            try:
-                session_id = self._decode_session_id(session_id)
-            except ValueError as e:
-                logger.error("Failed to decode session ID: %s", e)
-                return
-
-        session_key, session_id = session_id.split(":", 1)
-        self._redis.hdel(f"{self.session_path}:{session_key}", session_id)
-        logger.debug("Session %s deleted successfully.", session_id)
+        decoded_session_key = self._decode_session_id_if_needed(
+            session_id
+        ).split(":", 1)
+        self._redis.hdel(
+            f"{self.session_path}:{decoded_session_key[0]}",
+            session_id,
+        )
 
     def clear(self, session_key: str) -> None:
         """Clear all sessions for a given session key.
@@ -160,25 +143,32 @@ class RedisSessions(BaseSessionModule):
             session_id (str): The ID of the session to update.
             data (dict): The new data to be stored in the session.
         """
-        if hasattr(self, "_code_session_key"):
-            try:
-                session_id = self._decode_session_id(session_id)
-            except ValueError as e:
-                logger.error("Failed to decode session ID: %s", e)
-                return
-
-        session_key, session_id_ = session_id.split(":", 1)
-        if self.ttl:
-            data["_session_exp"] = (
-                datetime.datetime.now().timestamp() + self.ttl
+        decoded_session_key = self._decode_session_id_if_needed(
+            session_id
+        ).split(":", 1)
+        if not self.get(session_id):
+            raise SessionNotFoundError(
+                f"Session with ID {session_id} not found."
             )
+
         self._redis.hset(
-            name=f"{self.session_path}:{session_key}",
-            key=session_id_,
+            name=f"{self.session_path}:{decoded_session_key[0]}",
+            key=session_id,
             value=json.dumps(data),
         )
-        logger.debug("Session %s updated successfully.", session_id_)
+        logger.debug("Session %s updated successfully.", session_id)
 
+        if self.ttl:
+            self._redis.hexpire(
+                f"{self.session_path}:{decoded_session_key[0]}",
+                self.ttl,
+                session_id,
+            )
+            logger.debug(
+                "TTL for session %s reset to %d seconds.", session_id, self.ttl
+            )
+
+    # TODO: Optimize this method
     def rework(self, session_id: str) -> str:
         """Rework a session and return its new ID.
 
@@ -188,23 +178,40 @@ class RedisSessions(BaseSessionModule):
         Returns:
             str: The new session ID.
         """
-        session_id_ = session_id
-        if hasattr(self, "_code_session_key"):
-            try:
-                session_id_ = self._decode_session_id(session_id)
-            except ValueError as e:
-                SessionNotFoundError(f"Failed to decode session ID: {e}")
-
-        session_key, old_session_id = session_id_.split(":", 1)
-        data = self.get(session_id)
-
-        if not data:
+        decoded_session_key = self._decode_session_id_if_needed(
+            session_id
+        ).split(":", 1)
+        session_data = self.get(session_id)
+        if not session_data:
             raise SessionNotFoundError(
                 f"Session with ID {session_id} not found."
             )
 
-        new_session_id = self.create(session_key, data)
-        self.delete(session_id)
-        logger.debug("Session %s reworked to %s.", session_id, new_session_id)
+        new_session_id = self._encode_session_id_if_needed(
+            f"{decoded_session_key[0]}:{self.id}"
+        )
+        self._redis.hset(
+            name=f"{self.session_path}:{decoded_session_key[0]}",
+            key=new_session_id,
+            value=json.dumps(session_data),
+        )
+        logger.debug(
+            "Session %s reworked to new ID %s successfully.",
+            session_id,
+            new_session_id,
+        )
 
+        if self.ttl:
+            self._redis.hexpire(
+                f"{self.session_path}:{decoded_session_key[0]}",
+                self.ttl,
+                new_session_id,
+            )
+            logger.debug(
+                "TTL for new session %s set to %d seconds.",
+                new_session_id,
+                self.ttl,
+            )
+
+        self.delete(session_id)
         return new_session_id
