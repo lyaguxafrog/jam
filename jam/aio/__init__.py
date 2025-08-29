6da423c1585656897bfde91dae7a33eed9e98066
Adrian Makridenko <adrianmakridenko@duck.com>
2025-08-29 03:20:03 +0300

    [~] Version

--- jam/aio/__init__.py
+++ jam/aio/__init__.py
@@ -1,14 +1,10 @@
 # -*- coding: utf-8 -*-
 
 """
-JAM - Universal auth* library
-
-Source code: https://github.com/lyaguxafrog/jam
-Documentation: https://jam.makridenko.ru
+Async jam version.
 """
 
 from jam.aio.instance import Jam
 
 
-__version__ = "1.0.5"
 __all__ = ["Jam"]
