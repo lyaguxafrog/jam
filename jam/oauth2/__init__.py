24dd507a3461a8b25d1bbfd4d6d3be4ab1b23d8c
Adrian Makridenko <adrianmakridenko@duck.com>
2025-10-07 14:10:05 +0300

    [+] Github provider

--- jam/oauth2/__init__.py
+++ jam/oauth2/__init__.py
@@ -3,6 +3,7 @@
 """OAuth2 module."""
 
 from .__abc_oauth2_repo__ import BaseOAuth2Client
+from .builtin.github import GitHubOAuth2Client
 
 
-__all__ = ["BaseOAuth2Client"]
+__all__ = ["BaseOAuth2Client", "GitHubOAuth2Client"]
