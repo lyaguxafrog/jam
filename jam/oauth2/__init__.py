0ba11232df3c02816badde47742e118e62e72ed7
Adrian Makridenko <adrianmakridenko@duck.com>
2025-10-07 14:58:42 +0300

    [+] GitLab provider

--- jam/oauth2/__init__.py
+++ jam/oauth2/__init__.py
@@ -4,6 +4,8 @@
 
 from .__abc_oauth2_repo__ import BaseOAuth2Client
 from .builtin.github import GitHubOAuth2Client
+from .builtin.gitlab import GitLabOAuth2Client
+from .builtin.google import GoogleOAuth2Client
 
 
 __all__ = ["BaseOAuth2Client", "GitHubOAuth2Client"]
