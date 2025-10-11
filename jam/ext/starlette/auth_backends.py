f7a5c4793b8c11597ee4df6f5fa6baeb543a16b9
Adrian Makridenko <adrianmakridenko@duck.com>
2025-10-12 01:44:34 +0300

    [+] Session backend

--- jam/ext/starlette/auth_backends.py
+++ jam/ext/starlette/auth_backends.py
@@ -23,7 +23,7 @@ class JWTBackend(AuthenticationBackend):
     def __init__(
         self,
         jam: BaseJam,
-        cookie_name: Optional[str],
+        cookie_name: Optional[str] = None,
         header_name: Optional[str] = "Authorization",
     ) -> None:
         """Constructor.
@@ -60,6 +60,54 @@ class JWTBackend(AuthenticationBackend):
                 self._jam.verify_jwt_token(
                     token=token, check_exp=True, check_list=self.__use_list
                 )
+            )
+        except Exception as e:
+            logger.warning(f"Token verify error: {e}")
+            raise AuthenticationError("Token verification failed.")
+
+        return AuthCredentials(["authenticated"]), Payload(payload=payload)
+
+
+class SessionBackend(AuthenticationBackend):
+    """Sessions backend for starlette."""
+
+    def __init__(
+        self,
+        jam: BaseJam,
+        cookie_name: Optional[str] = "sessionId",
+        header_name: Optional[str] = None,
+    ) -> None:
+        """Constructor.
+
+        Args:
+            jam (BaseJam): Jam instance
+            cookie_name (str | None): Session id cookie name
+            header_name (str | None): Session id header name
+        """
+        self._jam = jam
+        self.cookie_name = cookie_name
+        self.header_name = header_name
+
+    async def authenticate(
+        self, conn: HTTPConnection
+    ) -> Optional[tuple[AuthCredentials, BaseUser]]:
+        """Starlette authentication handler."""
+        session_id = None
+
+        if self.cookie_name:
+            session_id = conn.cookies.get(self.cookie_name)
+
+        if not session_id and self.header_name:
+            header = conn.headers.get(self.header_name)
+            if header and header.startswith("Bearer "):
+                session_id = header.split("Bearer ")[1]
+
+        if not session_id:
+            return None
+
+        try:
+            payload: dict[str, Any] = await await_maybe(
+                self._jam.get_session(session_id)
             )
         except Exception as e:
             logger.warning(f"Token verify error: {e}")
