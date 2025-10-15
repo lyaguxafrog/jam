f5c4006826777823f29c5c1b1b72b33a3919bb3d
Adrian Makridenko <adrianmakridenko@duck.com>
2025-10-16 01:59:28 +0300

    [~] Move BaseEncoder to root

--- src/jam/paseto/__abc_paseto_repo__.py
+++ src/jam/paseto/__abc_paseto_repo__.py
@@ -64,19 +64,3 @@ class BasePASETO(ABC):
             dict[str, Any]: Payload
         """
         raise NotImplementedError
-
-
-class BaseEncoder(ABC):
-    """Base encoder instance."""
-
-    @classmethod
-    @abstractmethod
-    def dumps(cls, var: dict[str, Any]) -> bytes:
-        """Dump dict."""
-        raise NotImplementedError
-
-    @classmethod
-    @abstractmethod
-    def loads(cls, var: Union[str, bytes]) -> dict[str, Any]:
-        """Load json."""
-        return json.loads(var)
