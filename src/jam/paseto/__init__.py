39bacc435650a1fbfa4440eb8b1cecb91acbe3d5
Adrian Makridenko <adrianmakridenko@duck.com>
2025-10-16 18:41:05 +0300

    [+] v1 paseto to import

--- src/jam/paseto/__init__.py
+++ src/jam/paseto/__init__.py
@@ -3,6 +3,7 @@
 """PASETO auth* tokens."""
 
 from .__abc_paseto_repo__ import PASETO, BasePASETO
+from .v1 import PASETOv1
 
 
-__all__ = ["PASETO", "BasePASETO"]
+__all__ = ["PASETO", "BasePASETO", "PASETOv1"]
