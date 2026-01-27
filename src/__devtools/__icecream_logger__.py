91a7f3ac20b8589d1fc6392db47c409fa8938e96
Adrian Makridenko <adrianmakridenko@duck.com>
2026-01-28 00:58:44 +0300

    [~] Rename devtools

--- src/__devtools/__icecream_logger__.py
+++ src/__devtools/__icecream_logger__.py
@@ -10,7 +10,12 @@ from src.jam.logger import BaseLogger
 class IcecreamLogger(BaseLogger):
     """IcecreamLogger is a logger that uses the icecream library to log messages."""
 
-    def __init__(self, log_level: Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"] = "INFO"):
+    def __init__(
+        self,
+        log_level: Literal[
+            "DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"
+        ] = "INFO",
+    ):
         """Initialize the logger."""
         self.log_level = log_level
 
