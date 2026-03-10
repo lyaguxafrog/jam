adfdcf7650129f6162d674dc3bb93b50fc860ec9
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-10 19:43:31 +0300

    [*] Plugin draft

--- src/jam/ext/litestar/__init__.py
+++ src/jam/ext/litestar/__init__.py
@@ -5,9 +5,3 @@ Litestar integration.
 
 Litestar docs: https://docs.litestar.dev
 """
-
-from .plugins import JamPlugin, JWTPlugin, SessionsPlugin
-from .value import Auth, User
-
-
-__all__ = ["JamPlugin", "JWTPlugin", "User", "Auth", "SessionsPlugin"]
--- src/jam/ext/litestar/plugins.py
+++ src/jam/ext/litestar/plugins.py
@@ -3,28 +3,52 @@
 from collections.abc import Callable
 from typing import Any
 
+from litestar.config.app import AppConfig
+from litestar.di import Provide
 from litestar.plugins import InitPlugin
 
+from jam.exceptions import JamLitestarPluginConfigError
+from jam.jwt import JWT
 from jam.utils.config_maker import GENERIC_POINTER, __config_maker__
 
 
 class BasePlugin(InitPlugin):
     """Base Litestar plugin."""
 
-    __AUTH_MODULE: Callable
+    _AUTH_MODULE: Callable
 
-    def __init__(
+    def _setup_config(self, config: dict[str, Any]) -> None:
+        self._auth = self._AUTH_MODULE(**config)
+
+
+class JamJWTPlugin(BasePlugin):
+    """JWT plugin for litestar."""
+
+    _AUTH_MODULE = JWT
+
+    def __init__(  # noqa
         self,
         config: dict[str, Any] | str | None = None,
         pointer: str = GENERIC_POINTER,
+        cookie_name: str | None = None,
+        header_name: str | None = None,
         **kwargs,
     ) -> None:
-        """Initialize the plugin.
+        if not cookie_name and not header_name:
+            raise JamLitestarPluginConfigError(
+                message="Cookie name and header name cannot be both None.",
+                details={
+                    "cookie_name": cookie_name,
+                    "header_name": header_name,
+                },
+            )
 
-        Args:
-            config (dict[str, Any] | str | None): Jam config.
-            pointer (str): Pointer to the config.
-            **kwargs: Additional keyword arguments for the auth module.
-        """
-        _config = __config_maker__(config, pointer) if config else None
-        self.__auth = self.__AUTH_MODULE(**(_config if _config else kwargs))
+        _config: dict[str, Any] | None = (
+            (__config_maker__(config, pointer)) if config else None
+        )
+
+    def on_app_init(self, app_config: AppConfig) -> AppConfig:  # noqa: D102
+        app_config.dependencies["jwt"] = Provide(
+            lambda: self._auth, sync_to_thread=True
+        )
+        return app_config
