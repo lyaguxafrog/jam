ef1d74973a1b1ba91141624f470bf543c9ce78fd
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-11 16:51:04 +0300

    [~] inits

--- src/jam/ext/litestar/__init__.py
+++ src/jam/ext/litestar/__init__.py
@@ -5,3 +5,19 @@ Litestar integration.
 
 Litestar docs: https://docs.litestar.dev
 """
+
+from .objects import (
+    BaseUser,
+    SimpleUser,
+    Token
+)
+
+from .plugins import BasePlugin, JamJWTPlugin
+
+__all__ = [
+    "BaseUser",
+    "SimpleUser",
+    "Token",
+    "BasePlugin",
+    "JamJWTPlugin",
+]
