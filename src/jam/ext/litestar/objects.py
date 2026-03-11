46dd98a17276f3493d304813f0002f0deb827f9a
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-11 16:50:36 +0300

    [+] Simple user

--- src/jam/ext/litestar/objects.py
+++ src/jam/ext/litestar/objects.py
@@ -17,11 +17,12 @@ class Token:
     token: str | None
 
 
+@dataclass
 class BaseUser(ABC):
     """Abstract user model for AuthResult."""
 
-    @abstractmethod
     @classmethod
+    @abstractmethod
     def from_payload(cls, payload: dict[str, Any]) -> User:
         """Build model from payload.
 
@@ -29,3 +30,15 @@ class BaseUser(ABC):
             payload (dict[str, Any]): Payload for your data.
         """
         raise NotImplementedError
+
+
+@dataclass
+class SimpleUser(BaseUser):
+    """Simple user with payload."""
+
+    payload: dict[str, Any]
+
+    @classmethod
+    def from_payload(cls, payload: dict[str, Any]) -> SimpleUser:
+        """Set payload=payload."""
+        return cls(payload)
