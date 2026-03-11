1ea2fc5b7708ee03b4ccd7e445ab6669e1e83f5b
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-11 16:50:55 +0300

    [*] JWT Plugin

--- src/jam/ext/litestar/plugins.py
+++ src/jam/ext/litestar/plugins.py
@@ -8,6 +8,7 @@ from litestar.di import Provide
 from litestar.plugins import InitPlugin
 
 from jam.exceptions import JamLitestarPluginConfigError
+from jam.ext.litestar.objects import BaseUser
 from jam.jwt import JWT
 from jam.utils.config_maker import GENERIC_POINTER, __config_maker__
 
@@ -33,6 +34,7 @@ class JamJWTPlugin(BasePlugin):
         cookie_name: str | None = None,
         header_name: str | None = None,
         middleware: bool = True,
+        middleware_user: type[BaseUser] | None = None,
         **kwargs,
     ) -> None:
         if middleware and (not cookie_name and not header_name):
@@ -43,17 +45,36 @@ class JamJWTPlugin(BasePlugin):
                     "header_name": header_name,
                 },
             )
+        if middleware and not middleware_user:
+            raise JamLitestarPluginConfigError(
+                message="Middleware user cannot be None when middleware is True.",
+                details={
+                    "middleware_user": middleware_user,
+                },
+            )
 
         _config: dict[str, Any] | None = (
             __config_maker__(config, pointer) if config else None
         )
 
-        # TODO: Middleware setup
-
         self._auth = self.MODULE(**(_config if _config else kwargs))
+        self._middleware = None
+
+        if middleware:
+            from jam.ext.litestar.middleware import JWTMiddleware
+
+            _middleware = JWTMiddleware
+            _middleware.COOKIE_NAME = cookie_name
+            _middleware.HEADER_NAME = header_name
+            _middleware.AUTH_MODULE = self._auth
+            _middleware.USER = middleware_user  # type: ignore
+            self._middleware = _middleware
 
     def on_app_init(self, app_config: AppConfig) -> AppConfig:  # noqa
         app_config.dependencies["jwt"] = Provide(
             lambda: self._auth, sync_to_thread=True
         )
+        app_config.middleware.append(
+            self._middleware
+        ) if self._middleware else None
         return app_config
