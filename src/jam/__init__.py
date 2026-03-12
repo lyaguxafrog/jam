359424d014cef61a227e2761793952b5f7df611c
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-12 21:48:51 +0300

    [~] Version

--- src/jam/__init__.py
+++ src/jam/__init__.py
@@ -12,5 +12,5 @@ from jam.encoders import JsonEncoder
 from jam.instance import Jam
 
 
-__version__ = "3.0.0b13.dev1"
+__version__ = "3.0.0b13.dev2"
 __all__ = ["Jam", "JsonEncoder", "BaseJam", "BaseEncoder"]
