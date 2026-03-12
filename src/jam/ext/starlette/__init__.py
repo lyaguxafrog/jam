5c21b735defea684574364fb970cb98d64563242
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-12 16:56:38 +0300

    [-] Delete old code

--- src/jam/ext/starlette/__init__.py
+++ src/jam/ext/starlette/__init__.py
@@ -1,12 +1,6 @@
 # -*- coding: utf-8 -*-
 
-"""
-Starlette integration.
+"""Starlette integration.
 
 Starlette docs: https://starlette.dev
 """
-
-from .auth_backends import JWTBackend, SessionBackend
-
-
-__all__ = ["JWTBackend", "SessionBackend"]
