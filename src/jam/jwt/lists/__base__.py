3be4a47df11aa2b6c0450322a509b0298af903fb
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-15 04:17:38 +0300

    [+] Add typing to base list

--- src/jam/jwt/lists/__base__.py
+++ src/jam/jwt/lists/__base__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from abc import ABC, abstractmethod
-from typing import Any, Literal
+from typing import Literal
 
 
 class BaseJWTList(ABC):
@@ -12,16 +12,16 @@ class BaseJWTList(ABC):
         self.__list_type__ = list_type
 
     @abstractmethod
-    def add(self, token: str) -> Any:
+    def add(self, token: str) -> None:
         """Method for adding token to list."""
         raise NotImplementedError
 
     @abstractmethod
-    def check(self, token: str) -> Any:
+    def check(self, token: str) -> bool:
         """Method for checking if a token is present in the list."""
         raise NotImplementedError
 
     @abstractmethod
-    def delete(self, token: str) -> Any:
+    def delete(self, token: str) -> None:
         """Method for removing a token from a list."""
         raise NotImplementedError
