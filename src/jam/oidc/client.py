a1675090ea80f6427fe0b191d6e6495a79280cc7
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-16 21:18:58 +0300

    f

--- src/jam/oidc/client.py
+++ src/jam/oidc/client.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 
+from typing import Any
+
+from jam.oauth2.client import OAuth2Client
 from jam.oidc.__base__ import BaseOpenIDConnectClient
 
 
-class OpenIDConnectClient(BaseOpenIDConnectClient):
+class OpenIDConnectClient(BaseOpenIDConnectClient, OAuth2Client):
     """OIDC Client."""
+
+    def get_userinfo(self, access_token: str) -> dict[str, Any]:
+        """Returns the user info for the OpenID Connect flow.
+
+        Args:
+            access_token (str): The access token to use for the user info request.
+
+        Returns:
+            dict[str, Any]: The user info.
+        """
+        ...
