c55c2a9538d838869fc3418bbb77991b4a593f03
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-26 00:37:41 +0300

    [+] JOSE Exceptions

--- src/jam/exceptions/jose.py
+++ src/jam/exceptions/jose.py
@@ -6,3 +6,18 @@ from .base import JamValidationError
 class JamJWSVerificationError(JamValidationError):
     default_message = "JWS signature verification failed."
     default_code = "jws.verification_error"
+
+
+class JamJWKValidationError(JamValidationError):
+    default_message = "JWK validation failed."
+    default_code = "jwk.validation_error"
+
+
+class JamJWEEncryptionError(JamValidationError):
+    default_message = "JWE encryption failed."
+    default_code = "jwe.encryption_error"
+
+
+class JamJWEDecryptionError(JamValidationError):
+    default_message = "JWE decryption failed."
+    default_code = "jwe.decryption_error"
