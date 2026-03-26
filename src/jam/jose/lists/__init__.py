218582ad958a126a3d26b3e76f536e91d556dd60
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-26 17:32:32 +0300

    [~] Inits

--- src/jam/jose/lists/__init__.py
+++ src/jam/jose/lists/__init__.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""Module for managing white and black lists."""
+"""Module for managing JWT black and white lists."""
 
-from .__base__ import BaseJWTList
+from jam.jose.lists.__base__ import BaseJWTList
+from jam.jose.lists.json import JSONList
+from jam.jose.lists.memory import MemoryList
+from jam.jose.lists.redis import RedisList
 
 
-__all__ = ["BaseJWTList"]
+__all__ = ["BaseJWTList", "JSONList", "MemoryList", "RedisList"]
