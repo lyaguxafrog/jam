9071b25cfc38bb1e992aad93057f318cd8dcc4a0
Adrian Makridenko <adrianmakridenko@duck.com>
2026-03-26 14:18:32 +0300

    [+] Init JOSE

--- src/jam/jose/__init__.py
+++ src/jam/jose/__init__.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 
 """JOSE tools."""
+
+from jam.jose.jwk import JWK, JWKOct, JWKSet, JWKRSA, JWKEC
+from jam.jose.jws import JWS
+from jam.jose.jwe import JWE
+from jam.jose.jwt import JWT
+
+__all__ = [
+    "JWK",
+    "JWKSet",
+    "JWS",
+    "JWE",
+    "JWT",
+    "JWKRSA",
+    "JWKEC",
+    "JWKOct",
+]
