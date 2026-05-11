838a9f77e77edbb2a72c291d652053d69bf3bb38
Adrian Makridenko <adrianmakridenko@duck.com>
2026-05-11 14:33:17 +0300

    [+] JOSE exceptions

--- src/jam/exceptions/jose.py
+++ src/jam/exceptions/jose.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from .base import JamValidationError
+from .base import JamConfigurationError, JamValidationError
 
 
 class JamJWSVerificationError(JamValidationError):
@@ -8,9 +8,34 @@ class JamJWSVerificationError(JamValidationError):
     default_code = "jws.verification_error"
 
 
+class JamJWSValidationError(JamValidationError):
+    default_message = "JWS validation failed."
+    default_code = "jws.validation_error"
+
+
+class JamJWSInvalidFormatError(JamValidationError):
+    default_message = "Invalid JWS format."
+    default_code = "jws.invalid_format"
+
+
+class JamJWSSigningError(JamValidationError):
+    default_message = "JWS signing failed."
+    default_code = "jws.signing_error"
+
+
 class JamJWKValidationError(JamValidationError):
     default_message = "JWK validation failed."
     default_code = "jwk.validation_error"
+
+
+class JamJWKInvalidKeyTypeError(JamValidationError):
+    default_message = "Unsupported JWK key type."
+    default_code = "jwk.invalid_key_type"
+
+
+class JamJWKMissingParameterError(JamValidationError):
+    default_message = "Missing required JWK parameter."
+    default_code = "jwk.missing_parameter"
 
 
 class JamJWEEncryptionError(JamValidationError):
@@ -21,3 +46,23 @@ class JamJWEEncryptionError(JamValidationError):
 class JamJWEDecryptionError(JamValidationError):
     default_message = "JWE decryption failed."
     default_code = "jwe.decryption_error"
+
+
+class JamJWEInvalidFormatError(JamValidationError):
+    default_message = "Invalid JWE format."
+    default_code = "jwe.invalid_format"
+
+
+class JamInvalidKeyTypeError(JamConfigurationError):
+    default_message = "Invalid key type."
+    default_code = "jose.invalid_key_type"
+
+
+class JamAlgorithmError(JamConfigurationError):
+    default_message = "Algorithm error."
+    default_code = "jose.algorithm_error"
+
+
+class JamInvalidPaddingError(JamValidationError):
+    default_message = "Invalid padding."
+    default_code = "jose.invalid_padding"
